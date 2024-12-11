72adf57d322a913fcad0603bf328d0416b823130
Dorislin01 <81360006t@gmail.com>
2024-12-11 16:48:31 +0800

    cc

---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private bool smoothFollow = true;
+    [SerializeField] private float smoothSpeed = 5f;
+
+    private void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        if (smoothFollow)
+        {
+            transform.position = Vector2.Lerp(transform.position, mousePosition, smoothSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = mousePosition;
+        }
+    }
+}
+
